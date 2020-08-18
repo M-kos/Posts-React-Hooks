ab890edc6f8151aaeceb8d7ceb9b2c90ad6faa0d
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-08-19 02:46:21 +0300

    Изменил условие

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -6,7 +6,7 @@ import { Post } from '../Post/Post'
 export const PostList = ({ posts, users }) => {
   let postListMap
 
-  if (Array.isArray(posts)) {
+  if (posts && posts.length) {
     postListMap = posts.map(({ id, userId, title = '', body = '' }) => {
       return <Post key={id} user={users[userId]} title={title} body={body} />
     })
