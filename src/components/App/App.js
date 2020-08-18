8f069eb596fde46e898c37df44f8919d42cf7583
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-08-19 00:26:10 +0300

    Добавил условия и компонет

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,22 @@ import React from 'react'
 
 import { useSetData } from '../../hooks/useSetData'
 import { StyledApp } from '../../styledComponents/styledComponents'
+import { PostList } from '../PostList/PostList'
 
 const App = () => {
   const [state] = useSetData()
+  const { posts, users, isLoading, isError } = state
 
-  return <StyledApp />
+  return (
+    <StyledApp>
+      {isError && <div>Something went wrong...</div>}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <PostList posts={posts} users={users} />
+      )}
+    </StyledApp>
+  )
 }
 
 export default App
