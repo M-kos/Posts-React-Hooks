56b5410b28740a1000a1f7f721e2c61380df048b
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-08-19 04:14:45 +0300

    Build for gh-pages

---
+++ docs/precache-manifest.0cd1f563f9feb3ac90354e67e3c7dd1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce9bad3a9723795df01772a2d6eb09e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06fb16ebd0c4af0441ed",
+    "url": "/static/js/2.d05d92cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d05d92cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce3e8a31905ab67285e8",
+    "url": "/static/js/main.b2fa09fe.chunk.js"
+  },
+  {
+    "revision": "f9fe0b540f36e8ebbdd5",
+    "url": "/static/js/runtime-main.7543070e.js"
+  }
+]);
